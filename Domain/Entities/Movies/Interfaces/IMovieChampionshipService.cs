96089361834ae4e2e5e5299184b61b4d7c209864
Lucas Frois <ffrois.lucas@gmail.com>
2020-10-03 16:31:08 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Entities/Movies/Interfaces/IMovieChampionshipService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities.Movies.Interfaces
+{
+    public interface IMovieChampionshipService
+    {
+        List<Movie> MatchMovies(List<Movie> movies);
+    }
+}
---
+++ Domain/Entities/Movies/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities.Movies
+{
+    public class Movie
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int ReleaseYear { get; set; }
+        public float Rate { get; set; }
+    }
+}
